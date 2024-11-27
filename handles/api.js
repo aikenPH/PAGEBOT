4abe67b96286db8879c7311c406eddf2d1607897
LyxnDev <168339425+LyxnDev@users.noreply.github.com>
2024-11-27 21:24:20 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -7,5 +7,5 @@ module.exports = {
   nethApi: "https://nethwieginedev.vercel.app",
   jonelApi: "https://ccprojectapis.ddns.net",
   canvasApi: "https://api-canvass.vercel.app",
-  markApi: "https://markdevs-last-api-vtjp.onrender.com",
+  markApi: "https://apis-markdevs69v2.onrender.com",
 };
